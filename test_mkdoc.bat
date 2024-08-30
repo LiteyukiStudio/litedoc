59c0b57e1fd2e5fff1b40e6a22f4a27faa6db287
snowykami <snowykami@outlook.com>
2024-08-31 06:03:55 +0800

    :sparkles: 新增命令行入口

--- test_mkdoc.bat
+++ test_mkdoc.bat
@@ -1,3 +1,3 @@
 chcp 65001
-python -m litedoc tests/test_modules/mbcp -o docdist/api -l zh-Hans -fd def -md def -cd 类 -cs
-python -m litedoc tests/test_modules/mbcp -o docdist/en/api -l en -fd def -md def -cs
+litedoc tests/test_modules/mbcp -o docdist/api -l zh-Hans -fd def -md def -cd 类 -cs
+litedoc tests/test_modules/mbcp -o docdist/en/api -l en -fd def -md def -cs
